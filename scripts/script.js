b4bea314c7d73ce72fbf1874b5f3d245a3aa8c2e
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-02-13 01:28:17 -0800

    Changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+
+  if(localStorage.getItem('ourItems') === null) {
+    console.log("Hello");
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => localStorage.setItem('ourItems', JSON.stringify(data)));
+  }
+  console.log("Hello2");
+  
 });
