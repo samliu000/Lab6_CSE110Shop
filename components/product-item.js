6dcbde40e42878a8407cf64ad2d35116413ff92c
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-02-13 23:37:03 -0800

    Finished

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,111 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+
+  static get observedAttributes() {
+    return ['img'];
+  }
+
+  constructor() {
+    super();
+
+    this.attachShadow({mode:'open'});
+    const wrapper = document.createElement('li');
+      wrapper.setAttribute('class','product');
+
+      //image
+      let itemImg = wrapper.appendChild(document.createElement('img'));
+      itemImg.id = "itemImg";
+      itemImg.src = this.hasAttribute('img') ? this.getAttribute('img') : 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg';
+      // itemImg.setAttribute('src', 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg');
+      itemImg.setAttribute('alt', 'Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops');
+
+      //title
+      let titleP = wrapper.appendChild(document.createElement('p'));
+      titleP.id = "titleP";
+      titleP.setAttribute('class', 'title');
+      titleP.textContent = "Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops";
+
+
+      //price
+      let priceP = wrapper.appendChild(document.createElement('p'));
+      priceP.id = "priceP";
+      priceP.setAttribute('class', 'price');
+      priceP.textContent = "$109.95";
+
+      //add cart button
+      let cartBtn = wrapper.appendChild(document.createElement('button'));
+      cartBtn.id = "cartBtn";
+      cartBtn.setAttribute('onclick', "alert('Added to Cart!')");
+      cartBtn.textContent = "Add to Cart";
+
+      let style = document.createElement('style');
+      style.textContent = `.price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+      
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+      
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+      
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+      
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+      
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }`;
+      this.shadowRoot.append(style,wrapper);
+  }
+
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,68 @@ window.addEventListener('DOMContentLoaded', () => {
   var items = localStorage.getItem('ourItems');
   var itemsJSON = JSON.parse(items);
 
+  console.log(itemsJSON);
+  let productList = document.getElementById('product-list');
+  let cartCount = document.getElementById('cart-count');
+
+  // restore count
+  let count;
+  if(localStorage.getItem('count') === null) {
+    count = 0;
+    localStorage.setItem('count', JSON.stringify(count));
+  }
+  count = JSON.parse(localStorage.getItem('count'));
+  cartCount.textContent = count;
+
+  // added items
+  let added;
+  if(localStorage.getItem('addedItems') === null) {
+    added = ["","","","","","","","","","","","","","","","","","","",""];
+    localStorage.setItem('addedItems', JSON.stringify(added));
+  }
+  added = JSON.parse(localStorage.getItem('addedItems'));
+
+
+  // go through all JSON items
+  for(let i = 0; i< itemsJSON.length; i++) {
+
+    // create custom element
+    let currProduct = document.createElement('product-item');
+
+    // set image, alt, price, and title
+    currProduct.shadowRoot.getElementById('itemImg').src=itemsJSON[i].image;
+    currProduct.shadowRoot.getElementById('itemImg').alt=itemsJSON[i].title;
+    currProduct.shadowRoot.getElementById('titleP').textContent=itemsJSON[i].title;
+    currProduct.shadowRoot.getElementById('priceP').textContent="$" + itemsJSON[i].price;
+
+    if(added[itemsJSON[i].id] === "y") {
+      currProduct.shadowRoot.getElementById('cartBtn').textContent = "Remove from Cart";
+    }
+    // onclick for the add to cart/ remove from cart button
+    currProduct.shadowRoot.getElementById('cartBtn').onclick = () => {
+      console.log("clicked" + currProduct.shadowRoot.getElementById('cartBtn').textContent);
+      if(currProduct.shadowRoot.getElementById('cartBtn').textContent == "Add to Cart") {
+        currProduct.shadowRoot.getElementById('cartBtn').textContent = "Remove from Cart";
+        count++;
+        cartCount.textContent = count;
+        added[itemsJSON[i].id] = "y";
+        localStorage.setItem('addedItems', JSON.stringify(added));
+        localStorage.setItem('count', JSON.stringify(count));
+      }
+      else {
+        currProduct.shadowRoot.getElementById('cartBtn').textContent = "Add to Cart";
+        count--;
+        cartCount.textContent = count;
+        added[itemsJSON[i].id] = "";
+        localStorage.setItem('addedItems', JSON.stringify(added));
+        localStorage.setItem('count', JSON.stringify(count));
+      }
+    }
+
+    console.log(added);
+    productList.appendChild(currProduct);
+  }
   
   
 });
+
